adeec916228b7a5578923ee60bf2afccfc1c92fa
Guilherme Luis <guile.hm@hotmail.com>
2018-08-05 15:05:44 -0300

    Create core urls

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+]
