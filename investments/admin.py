9c7aa57ec88996d8c79da9f58d1cf1730820e993
Guilherme Luis <guile.hm@hotmail.com>
2018-08-05 16:25:22 -0300

    Update admin

--- investments/admin.py
+++ investments/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from investments.models import Investment, Profitable
+
 
 # Register your models here.
+@admin.register(Investment)
+class InvestmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'title', 'nature', 'broker', 'due_date', 'representativeness'
+    )
+    list_filter = (
+        'user', 'nature', 'broker',
+    )
+    search_fields = (
+        'user', 'title', 'nature',
+    )
+
+
+@admin.register(Profitable)
+class ProfitableAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'ipca', 'cdi', 'selic', 'flat_rate',
+    )
