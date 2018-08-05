d1d4a75676fb8d8f47e062ae8cd83825d44ef560
Guilherme Luis <guile.hm@hotmail.com>
2018-08-05 15:05:49 -0300

    Create core views

---
+++ core/views.py
@@ -0,0 +1,50 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import redirect, render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            authenticated_user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            login(request, authenticated_user)
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Olá, {}, seu login foi efetuado com sucesso'.format(request.user)
+            )
+            return redirect('core:index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'core/login.html', {'login_form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('core:index'))
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        context = {'form': form}
+        if form.is_valid():
+            user = form.save()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('core:index')
+        else:
+            if form.errors:
+                return render(request, 'core/register.html', context)
+    else:
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, 'core/register.html', context)
